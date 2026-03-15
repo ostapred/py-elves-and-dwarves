2e5aaec8d56bd8c9e2506bdf9657eff0a1200c2a
Andrii Ostapenko <ostapred3@gmail.com>
2026-03-15 19:28:00 +0100

    Fixed loop

--- app/main.py
+++ app/main.py
@@ -7,9 +7,11 @@ def calculate_team_total_rating(players: list[Player]) -> int:
     return sum(player.get_rating() for player in players)
 
 
-def elves_concert(participants: list[Elf]) -> str:
-    return [participant.play_elf_song() for participant in participants]
+def elves_concert(participants: list[Elf]) -> None:
+    for player in participants:
+        player.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list[Dwarf]) -> str:
-    return [dwarf.eat_favourite_dish() for dwarf in dwarves]
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
