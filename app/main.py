7a9d512362f7dee956b3431740e2c09f12272725
Andrii Ostapenko <ostapred3@gmail.com>
2026-03-15 18:59:52 +0100

    Fixed main

--- app/main.py
+++ app/main.py
@@ -4,17 +4,12 @@ from app.players.dwarves.dwarf import Dwarf
 
 
 def calculate_team_total_rating(players: list[Player]) -> int:
-    team_toral_rating = 0
-    for player in players:
-        team_toral_rating += player.get_rating()
-    return team_toral_rating
+    return sum(player.get_rating() for player in players)
 
 
-def elves_concert(participants: list[Elf]) -> list[str]:
-    all_songs = [participant.play_elf_song() for participant in participants]
-    return all_songs
+def elves_concert(participants: list[Elf]) -> str:
+    return [participant.play_elf_song() for participant in participants]
 
 
-def feast_of_the_dwarves(dwarves: list[Dwarf]) -> list[str]:
-    dishes = [dwarf.eat_favourite_dish() for dwarf in dwarves]
-    return dishes
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> str:
+    return [dwarf.eat_favourite_dish() for dwarf in dwarves]
